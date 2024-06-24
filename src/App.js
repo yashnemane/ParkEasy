454c91b08ecbfa5c24e107cd513bd26f6fcef86a
Yashkumar Nemane <yash.nemane@infinitylearn.com>
2024-06-24 11:31:39 +0530

    commit 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './assests/styles/index.css';
 import './assests/styles/App.css';
 import Header from './components/Header.js';
 import Homepage from './pages/Homepage.js';
+import NewsLetter from './components/NewsLetter.js'
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
       <Homepage/>
+      <NewsLetter/>
+      <Footer/>
     </div>
   );
 }
--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../assests/styles/header.css';
-//import parkingImage1 from '../assests/images/homepagelist4.jpg'
+import parkingImage1 from '../assests/images/homepagelist4.jpg'
 import {PARKING_APP_NAME} from '../config/constants'
 import '../assests/styles/homepage.css'
 
@@ -8,8 +8,11 @@ export default function PhotoGallery () {
   return (
     <div className='below-header'>
     <h2>Welcome to <span class="italic">{PARKING_APP_NAME}</span></h2>
+    <div className="image-list">
+        <img src={parkingImage1} alt="Parking Spot 1" />
+    </div>
     <div className="title-line">
-      <p>Trusted by parking operators across the biryani capital.</p>
+      <p>Trusted by parking operators across the biryani capital of india.</p>
     </div>
     </div>
   );
@@ -18,5 +21,4 @@ export default function PhotoGallery () {
 /*<div className="image-list">
         <img src={parkingImage1} alt="Parking Spot 1" />
       </div>
-      <h2>Welcome to <span class="italic">{PARKING_APP_NAME}</span></h2>
       */
